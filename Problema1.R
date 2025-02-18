906ab2c1a1dcb6ef032f417f757e774ff9cda528
Eloi-Gil <eloi.gil.fernandez@estudiantat.upc.edu>
2025-02-18 09:30:55 +0100

    Update Problema1.R

--- Problema1.R
+++ Problema1.R
@@ -1,3 +1,34 @@
 #Codigo para problema 1
+mis_dades <- mtcars
+mis_dades
+dim(mis_dades)
+names(mis_dades)
+mis_dades$qsec
+x <- mis_dades$qsec
+x
+sum(x)/length(x)
+mean(x)
 
-plot(1:5)
+mis_dades$drat
+sort(mis_dades$drat)
+median(mis_dades$drat)
+
+quantile(mis_dades$drat,0.25)
+quantile(mis_dades$drat,0.75)
+
+quantile(mis_dades$mpg,0.18)
+
+x <- quantile(mis_dades$cyl,0.25)
+y <- quantile(mis_dades$cyl,0.75)
+sort(mis_dades$cyl)
+y-x
+IQR(mis_dades$cyl)
+
+boxplot(mis_dades$cyl)
+
+sd(mis_dades$cyl)
+var(mis_dades$qsec)
+#var otra forma
+x <- mis_dades$qsec
+(x-mean(x))^2
+sum((x-mean(x))^2)/(length(x)-1)
